a7d62ae180e1da62bff2c89f28040378e5c7b67d
alf1e <alfiewbarlow@gmail.com>
2022-10-01 09:25:34 +0100

    fix: guild role ids

--- cogs/beta.py
+++ cogs/beta.py
@@ -20,13 +20,13 @@ class BetaTesters(commands.Cog):
     beta_testers = SlashCommandGroup(
         "betab",
         "Beta Testers Tester commands",
-        checks=[commands.has_role(1005262215378636840)],
+        checks=[commands.has_role(1015004274121515081)],
     )
 
     @beta_admin.command()
     async def add(self, ctx: discord.ApplicationContext, user: discord.Member):
         # Add user to beta testers role
-        await user.add_roles(ctx.guild.get_role(1005262215378636840))
+        await user.add_roles(ctx.guild.get_role(1015004274121515081))
         # Add user to beta testers collection
         self.db.testers.insert_one({"id": user.id, "account_created": False, "license_created": False})
         await ctx.respond(f"{user.mention} has been added to beta testers")
@@ -34,7 +34,7 @@ class BetaTesters(commands.Cog):
     @beta_admin.command()
     async def remove(self, ctx: discord.ApplicationContext, user: discord.Member):
         # Remove user from beta testers role
-        await user.remove_roles(ctx.guild.get_role(1005262215378636840))
+        await user.remove_roles(ctx.guild.get_role(1015004274121515081))
         # Remove user from beta testers collection
         self.db.testers.delete_one({"id": user.id})
         await ctx.respond(f"{user.mention} has been removed from beta testers")
@@ -42,7 +42,7 @@ class BetaTesters(commands.Cog):
     @beta_admin.command()
     async def init_db(self, ctx: discord.ApplicationContext):
         # Insert all beta testers into database
-        for member in ctx.guild.get_role(1005262215378636840).members:
+        for member in ctx.guild.get_role(1015004274121515081).members:
             self.db.testers.insert_one({"id": member.id, "account_created": False, "license_created": False})
 
 
